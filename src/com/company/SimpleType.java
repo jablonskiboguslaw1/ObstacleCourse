e7bebb4d0fe3500fc252b706b6d15be0ae400eb4
jablonskiboguslaw1 <jablonski.boguslaw@gmail.com>
2022-01-31 01:00:18 +0100

    Two tasks for beginners

---
+++ src/com/company/SimpleType.java
@@ -0,0 +1,37 @@
+package com.company;
+
+/*
+1. Print information "=====variables declaration====="
+2. Declare variables one of each listed type.
+ byte, short, int, long, double , float, boolean and char
+ ---protip--- remember that declaration is not the same that initialization.
+
+3. Print information "=====variables declared but not initialized yet======".
+
+4. Initialize all variables mentioned above with some values.
+
+5. Print information  "=====variables initialized======".
+    5a. Print them all one by one.
+
+6. Add int and long , use Alt+Enter to assign it to newly crated variable.
+6a. Add int/long and double and float , use Alt+Enter to assign it to newly crated variable.
+
+7. Add byte and short variable , use Alt+Enter to assign it to newly crated variable.
+
+8. Declare and initialize two int values,  one with value of 15, second with value of 4. Assign division to variable and print it.
+
+9. Try to add char to some int value. Print the sum.
+    9a. Cast variable to char (precede variable name  by "(char)" inside 'sout' command).
+
+
+
+*/
+public class SimpleType {
+
+    public static void main(String[] args) {
+
+//start here
+
+
+    }
+}
---
+++ src/com/company/StringManipulator.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class StringManipulator {
+
+    /*
+    1. Initialize example string by this value -> "         this is an example string       "  (copy/paste whole string between "");
+
+    2.Take new variable and call method trim() and assign value to new variable.
+
+    3. Take new variable and call replace() with params "thi" and "w", then assign value to new variable.
+
+    4. Take new variable and call replaceFirst() with params "n"  and "nn", then assign value to new variable.
+
+    5. Take new variable and call replaceAll() with params "*s*a*\\s" (regex- regular expression) and "" (empty string). Assign value to new variable.
+
+    6. Take new variable and call substring() form 0 ( java counts from '0' , 0 means "first index/beginning") to 5). Assign value to new variable.
+
+    7.  Take new variable and call upper() form 0 ( java counts from '0' , 0 means "first index/beginning") to 5). Assign value to new variable.
+
+    8.  Take new variable and call concat() with param ("R!!!"). Assign value to new variable.
+
+    9. Print the last crated variable.
+
+    10.Try to make the same sequentially calling methods one by one without breaking line and declaring new variables. Like ->  System.out.println(example.call()call(params).call().etc....call())
+
+    Experiment with String class API ( Application Programming Interface-> means all methods shared by class, library, program...)
+    */
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println("START");
+
+        //start your code here
+
+    }
+
+}
